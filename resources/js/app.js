bf44b85250a982f9176425b85dbbbf2fe7b029da
Ntd 1683 <ntdvlog1683@gmail.com>
2023-05-03 00:26:08 +0700

    Install Template: Fix Bug

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,8 @@
 import './bootstrap';
-import './main'
 import TranslationPlugin from './plugins/Translation'
 
 window.$ = window.jQuery = require('jquery');
 window.Popper = require('popper.js');
 require('bootstrap');
-require('main')
 
 app.use(TranslationPlugin)
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,11 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-    //
-    ])
-    .sass('resources/scss/app.scss', 'public/css')
+mix
+    .js('resources/js/main.js', 'public/js')
+    .js('resources/js/app.js', 'public/js')
+    .sass('resources/scss/styles.scss', 'public/css')
     .disableNotifications()
     .webpackConfig(require('./webpack.config'));
 
