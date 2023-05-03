fe979a8aca97b66b9b314532b61ec35a1dd5943f
Ntd 1683 <ntdvlog1683@gmail.com>
2023-05-04 01:31:25 +0700

    Install Template: Bug Menu Mobile

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,17 @@ window.$ = window.jQuery = require('jquery')
 window.Popper = require('popper.js')
 window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js')
 require('bootstrap')
+
+window.addEventListener('click', (e) => {
+    let app = $('#app');
+    let elm = e.target;
+    if(app.hasClass('app-sidebar-mobile-toggled')) {
+        if(
+            ! elm.classList.contains('app-sidebar-content')
+            && ! elm.classList.contains('menu-toggler')
+            && ! elm.classList.contains('bar')
+        ) {
+            app.removeClass('app-sidebar-mobile-toggled');
+        }
+    }
+});
