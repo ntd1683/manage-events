c82229a96bfd934399e016ca06ac2e0479ffc172
Nguyễn Tấn Dũng <93341713+ntd1683@users.noreply.github.com>
2023-05-05 01:19:02 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::get('/index', function () {
+    return view('index');
+})->name('index');
+
 Route::get('/Forget_Password', function () {
     return view('Forget_Password');
 })->name('Forget_Password');
