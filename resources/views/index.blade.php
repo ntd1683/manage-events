4ca23f1ac7ea1a47b1194a02507e953ede9867a4
Asa Nguyen <96124608+Asa-Nguyen@users.noreply.github.com>
2023-05-05 01:28:41 +0700

    Backup index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<x-layouts.master>
+    <!-- BEGIN #content -->
+    {{ Breadcrumbs::render('index') }}
+
+        <h1 class="page-header">
+            Starter Page <small>page header description goes here...</small>
+        </h1>
+
+        <p>
+            Start build your page here
+        </p>
+    <!-- END #content -->
+</x-layouts.master>
