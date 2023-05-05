89f574027f4b7723462ae26537c0ef02989cabfe
Ntd 1683 <ntdvlog1683@gmail.com>
2023-05-05 14:51:19 +0700

    Auth: login

---
+++ app/Http/Middleware/CheckLoginMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckLoginMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(Auth::guest()) {
+            return redirect()->route('login')->withErrors( 'User are not login !!!');
+        }
+
+        return $next($request);
+    }
+}
---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email', 'string'],
+            'password' => ['required', 'string'],
+        ];
+    }
+}
